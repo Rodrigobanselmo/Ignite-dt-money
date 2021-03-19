4775b163d7e2eb2cd0eb9b864d64098626e8deb8
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-03-19 12:21:35 -0300

    feat: polished add to project

--- src/components/NewTrasactionModal/styles.ts
+++ src/components/NewTrasactionModal/styles.ts
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import { darken, transparentize } from 'polished'
+import styled from 'styled-components';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
   h2 {
@@ -29,12 +29,12 @@ export const Container = styled.form`
     }
   }
 
-  button[type="submit"] {
+  button[type='submit'] {
     width: 100%;
     padding: 0 1.5rem;
     height: 4rem;
     background: var(--green);
-    color: #FFF;
+    color: #fff;
     border-radius: 0.25rem;
     border: 0;
     font-size: 1rem;
@@ -43,7 +43,7 @@ export const Container = styled.form`
     transition: filter 0.2s;
 
     &:hover {
-      filter: brightness(0.9)
+      filter: brightness(0.9);
     }
   }
 `;
@@ -52,7 +52,7 @@ export const TransactionTypeContainer = styled.div`
   margin: 1rem 0;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  gap: 0.5rem;  
+  gap: 0.5rem;
 `;
 
 interface RadioBoxProps {
@@ -62,18 +62,18 @@ interface RadioBoxProps {
 
 const colors = {
   green: '#33CC95',
-  red: '#E52E4D'
-}
+  red: '#E52E4D',
+};
 
 export const RadioBox = styled.button<RadioBoxProps>`
   height: 4rem;
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
 
-  background: ${(props) => props.isActive 
-  ? transparentize(0.9, colors[props.activeColor])
-  : 'transparent'
-  };
+  background: ${(props) =>
+    props.isActive
+      ? transparentize(0.9, colors[props.activeColor])
+      : 'transparent'};
 
   display: flex;
   align-items: center;

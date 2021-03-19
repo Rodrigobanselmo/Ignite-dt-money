e8641ba6cb1377b59d0ed04f344ad033dbb5dc55
Rodrigo Barbosa Anselmo <rodrigobanselmo@gmail.com>
2021-03-19 11:46:04 -0300

    feat: created modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React, { useState } from 'react';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { GlobalStyle } from './styles/global';
+import { NewTransactionModal } from './components/NewTrasactionModal';
 
-export const App: React.FC = () => (
-  <>
-    <Header />
-    <GlobalStyle />
-    <Dashboard />
-  </>
-);
+export const App: React.FC = () => {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false,
+  );
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+  return (
+    <>
+      <Header onOpenNewTranslationModal={handleOpenNewTransactionModal} />
+      <GlobalStyle />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <Dashboard />
+    </>
+  );
+};
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,22 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React from 'react';
 import logoImg from '../../assets/logo.svg';
 
 import { Container, Content } from './styles';
 
-export const Header: React.FC = () => {
+interface HeaderProps {
+  onOpenNewTranslationModal: () => void;
+}
+
+export const Header = ({ onOpenNewTranslationModal }: HeaderProps) => {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dtmoney" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTranslationModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
